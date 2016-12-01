11da9ff69df3c5c580f8463975f4926b5eba55f2
HappyGreenla <1582571231@qq.com>
2016-12-01 19:59:16 +0800

    input

---
+++ input.py
@@ -0,0 +1,13 @@
+#name=raw_input('What is your name?')
+#print('Hi ' + name)
+#name=raw_input('What is your name?')
+#age=raw_input('How old are you?')
+#print(name + ' is '+ age+ ' years old.')
+#dog_years =int(age)*7
+#print('You are '+ str(dog_years)+ ' old in dog years.')
+
+meal=raw_input('How much was your meal?')
+tip=float(meal)*.20
+print('You should tip $ '+str(tip))
+cost=tip+float(meal)
+print('You total cost would be $ '+str(cost))
