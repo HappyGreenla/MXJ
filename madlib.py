a44d4e3c4fff4706bbad90f3aff334ab3b9d1618
HappyGreenla <1582571231@qq.com>
2016-12-01 19:59:22 +0800

    madlib

---
+++ madlib.py
@@ -0,0 +1,39 @@
+
+print('Welcome to the Mad Lib Generator!')
+
+adj1=raw_input('Type in an adjective:')
+adj2=raw_input('Another adjective:')
+adj3=raw_input('And another adjective:')
+adj4=raw_input('One more adjective:')
+animal=raw_input('Plural animal(e.g.,puppies):')
+verb=raw_input('Now a verb:')
+noun=raw_input('Now a plural noun:')
+first_name=raw_input('The first name of a woman musician:')
+beverage=raw_input('A beverage:')
+body_part=raw_input('And finally,a plural body part:')
+
+print('\n')
+print("Excellent choices!Here's your story...")
+print("------------------------------------")
+print('There once was a '+adj1+' programmer named '+first_name)
+print('who wanted to build a '+adj2+' mobile app to')
+print('help '+adj3+' '+animal+' find new homes.')
+
+print('\n')
+print(first_name+' stayed up all night,drinking lots of')
+print('caffeinated '+beverage+' as she worked and worked')
+print('trying to complete her '+adj4+' program.')
+
+print('\n')
+print('Whenever '+first_name+' hit a dead end,she would')
+print('exclaim *'+noun+'*!')
+
+print('\n')
+print('But she was not discouraged,and after a quick break')
+print('to '+verb+' and clear her head,she got back to work.')
+
+print('\n')
+print('By morning,when the sun started to rise,she let out a')
+print('big yawn and stretched her '+body_part+'.')
+print('Finally,she was done.')
+print('--------------------------')
