ecb8f15e3c65e609d64fb3ddab8cce44ec744728
HappyGreenla <1582571231@qq.com>
2016-12-01 19:58:57 +0800

    booleans

---
+++ booleans.py
@@ -0,0 +1,22 @@
+#print('a' > 'z') # Expected outcome:False
+
+#print('z' > 'a') # Expected outcome:True
+
+#print('apples' > 'oranges') # Expected outcome:False
+
+#print('oranges' > 'apples') # Expected outcome:True
+
+#print('cat' > 'car') # Expected outcome:True
+
+#print('car' > 'cat') # Expected outcome:False
+
+#answer = int(raw_input('What is 1 + 1? '))
+#print(answer == 2)
+#gesture = 'rock'
+#print(gesture == 'rock' or gesture == 'paper' or gesture == 'scissors') # Expected outcome:Ture
+
+#gesture = 'pape'
+#print(gesture == 'rock' or gesture == 'paper' or gesture == 'scissors') # Expected outcome:False
+
+age=int(raw_input('How old are you?'))
+print(age>5 and <10)
