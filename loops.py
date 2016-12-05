28f766939eda165650e711a86ff42d1c8bb0b8d2
HappyGreenla <1582571231@qq.com>
2016-12-05 14:54:27 +0800

    loops

---
+++ loops.py
@@ -0,0 +1,10 @@
+#numbers = range(0,10)
+# Used Built-in Python function range() to produce a list: [0,1,2,3,4,5,6,7,8,9]
+
+#for counter in numbers:
+#    print '#' * counter
+
+#letter = 'aeiou'
+#letter = ['a','e','i','o','u']
+#for list in letter:
+#    print list
